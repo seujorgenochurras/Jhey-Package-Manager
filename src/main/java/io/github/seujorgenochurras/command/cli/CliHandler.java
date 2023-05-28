b2639c475fd17307a6f4ae3b98b5b2a4def89678
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2023-05-28 13:43:47 -0300

    fucking first release bro lets goooooooo

--- src/main/java/io/github/seujorgenochurras/command/cli/CliHandler.java
+++ src/main/java/io/github/seujorgenochurras/command/cli/CliHandler.java
@@ -3,6 +3,10 @@ package io.github.seujorgenochurras.command.cli;
 import io.github.seujorgenochurras.command.toolbox.CommandToolBox;
 import io.github.seujorgenochurras.command.ICommand;
 import io.github.seujorgenochurras.command.cli.utils.StringUtils;
+import io.github.seujorgenochurras.domain.GradlewBuildFile;
+import io.github.seujorgenochurras.mapper.gradlew.mapper.GradlewFileMapper;
+
+import java.io.File;
 
 import static io.github.seujorgenochurras.command.reflections.register.CommandRegister.COMMANDS;
 
@@ -21,7 +25,9 @@ public class CliHandler {
       String commandFlags = cliCommandSeparatedFromFlags[commandArgsIndex].trim();
 
       ICommand command = COMMANDS.get(commandName);
-      CommandToolBox toolBox = new CommandToolBox(command.flagFormatter().formatString(commandFlags));
+
+      GradlewBuildFile buildFile = GradlewFileMapper.mapFile(new File("build.gradle.kts"));
+      CommandToolBox toolBox = new CommandToolBox(command.flagFormatter().formatString(commandFlags), buildFile);
       command.invoke(toolBox);
    }
 }
--- src/main/java/io/github/seujorgenochurras/command/toolbox/CommandToolBox.java
+++ src/main/java/io/github/seujorgenochurras/command/toolbox/CommandToolBox.java
@@ -1,13 +1,18 @@
 package io.github.seujorgenochurras.command.toolbox;
 
 import io.github.seujorgenochurras.command.arg.CommandArgs;
+import io.github.seujorgenochurras.domain.GradlewBuildFile;
 
 public class CommandToolBox {
 
-   public CommandToolBox(CommandArgs commandArgs) {
-      this.commandArgs = commandArgs;
-   }
 
    public final CommandArgs commandArgs;
    public final CommandConsole commandConsole = new CommandConsole();
+
+   public final GradlewBuildFile gradlewBuildFile;
+
+   public CommandToolBox(CommandArgs commandArgs, GradlewBuildFile gradlewBuildFile) {
+      this.commandArgs = commandArgs;
+      this.gradlewBuildFile = gradlewBuildFile;
+   }
 }
