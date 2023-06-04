4fee79b3374c28f5b78384fccf07ae4c5bf696ea
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2023-06-04 15:20:17 -0300

    Added groovy and kotlin support

--- src/main/java/io/github/seujorgenochurras/command/cli/CliHandler.java
+++ src/main/java/io/github/seujorgenochurras/command/cli/CliHandler.java
@@ -3,10 +3,6 @@ package io.github.seujorgenochurras.command.cli;
 import io.github.seujorgenochurras.command.toolbox.CommandToolBox;
 import io.github.seujorgenochurras.command.ICommand;
 import io.github.seujorgenochurras.command.cli.utils.StringUtils;
-import io.github.seujorgenochurras.domain.GradlewBuildFile;
-import io.github.seujorgenochurras.mapper.gradlew.mapper.GradlewFileMapper;
-
-import java.io.File;
 
 import static io.github.seujorgenochurras.command.reflections.register.CommandRegister.COMMANDS;
 
@@ -26,8 +22,8 @@ public class CliHandler {
 
       ICommand command = COMMANDS.get(commandName);
 
-      GradlewBuildFile buildFile = GradlewFileMapper.mapFile(new File("build.gradle.kts"));
-      CommandToolBox toolBox = new CommandToolBox(command.flagFormatter().formatString(commandFlags), buildFile);
+
+      CommandToolBox toolBox = new CommandToolBox(command.flagFormatter().formatString(commandFlags));
       command.invoke(toolBox);
    }
 }
