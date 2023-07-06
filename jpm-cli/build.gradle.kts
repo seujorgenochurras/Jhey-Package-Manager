b1e6de0f11f4200fc51237c5da1981f90ba62f92
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2023-07-06 18:46:28 -0300

    Released alpha-0.4.0

--- jpm-cli/build.gradle.kts
+++ jpm-cli/build.gradle.kts
@@ -7,7 +7,6 @@ version = "unspecified"
 
 repositories {
     mavenCentral()
-    mavenLocal()
 }
 
 dependencies {
@@ -15,7 +14,7 @@ dependencies {
     implementation ("org.reflections:reflections:0.10.2")
     implementation ("ch.qos.logback:logback-classic:1.4.7")
     implementation ("com.google.code.gson:gson:2.10.1")
-    implementation ("io.github.seujorgenochurras:java-dependency-manager:0.4.3")
+    implementation ("io.github.seujorgenochurras:java-dependency-manager:0.6.0")
 }
 
 tasks.test {
--- jpm-cli/src/main/java/io/github/seujorgenochurras/Main.java
+++ jpm-cli/src/main/java/io/github/seujorgenochurras/Main.java
@@ -3,8 +3,6 @@ package io.github.seujorgenochurras;
 import io.github.seujorgenochurras.command.cli.CliHandler;
 import org.fusesource.jansi.AnsiConsole;
 
-import java.time.OffsetDateTime;
-
 public class Main {
     public static void main(String[] args) {
         AnsiConsole.systemInstall(); //needed because windows has no AnsiConsole by default
